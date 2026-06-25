849a5af8b7bbcd7b3627038eb08a686e124be5ee
kevin070628 <jin070628@gmail.com>
2026-06-25 13:40:50 +0900

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 💡 [필독] 이전에 구글 콘솔에서 발급받은 API 키를 아래 변수에 직접 넣어주세요!
-YOUTUBE_API_KEY = "방금_발급받은_AIzaSy_시작하는_API_키"
+YOUTUBE_API_KEY = "AIzaSyDZp9u4RRrZaaeP3MmfPvMHNXLYFSuwPc4"
 
 # 3대장 게임 데이터 고정
 YOUTUBE_CHANNELS = {
